108373bef77129bee2b747397a64f2b02fa2374d
Himanshu-Gautam20 <himanshugautam9696@gmail.com>
2025-06-27 16:22:19 +0530

    auth store

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import {create} from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+
+export const useAuthStore = create((set) => ({
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+    isCheckingAuth: true,
+    onlineUsers: [],
+    socket: null,
+
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get("/auth/check");
+            set({authUser: res.data});
+        } catch (error) {
+            console.log("Error checking auth:", error);
+            set({authUser: null});
+        } finally {
+            set({isCheckingAuth: false});
+        }
+    }
+}));
