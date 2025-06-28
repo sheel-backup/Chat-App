c04bb068dec14fcce60c84aba9ea5d5f0d81a989
Himanshu-Gautam20 <himanshugautam9696@gmail.com>
2025-06-28 18:22:55 +0530

    commit check

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
-import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -47,75 +47,72 @@ export const signup = async (req, res) => {
   }
 };
 
-
-
 export const login = async (req, res) => {
-  const { email, password }= req.body;
+  const { email, password } = req.body;
   try {
-    const user = await User.findOne({email});
-    if(!user) {
-      return res.status(400).json({message: "Invalid email or password"});
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
     }
 
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
-
-    if(!isPasswordCorrect) {
-      return res.status(400).json({message: "Invalid email or password"});
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
     }
 
     generateToken(user._id, res);
+
     res.status(200).json({
       _id: user._id,
-      fullName: user.fullName,    
-      email: user.email,    
+      fullName: user.fullName,
+      email: user.email,
       profilePic: user.profilePic,
-    })
-     
+    });
   } catch (error) {
     console.log("Error in login controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
-
-
 export const logout = (req, res) => {
   try {
-    res.cookie("jwt", "", {maxAge:0});
-  res.status(200).json({ message: "Logged out successfully" });
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     console.log("Error in logout controller", error.message);
-    res.status(500).json({ message: "Internal Server Error" }); 
-  }
-};
-
-
-export const updateProfile = async (req, res) => {
-  const {profilePic} = req.body;
-  try {
-    const userId = req.user._id;
-
-    if(!profilePic) {
-      return res.status(400).json({message: "Profile picture is required"});
-    }
-
-    const uploadResponse = await cloudinary.uploader.upload(profilePic);
-    const  updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true});
-
-    res.status(200).json(updatedUser);
-  } catch (error) {
-    console.log("Error in updateProfile controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile pic is required" });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("error in update profile:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export const checkAuth = (req, res) => {
   try {
     res.status(200).json(req.user);
-    
   } catch (error) {
     console.log("Error in checkAuth controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
-}
+};
--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -1,28 +1,25 @@
 import mongoose from "mongoose";
 
-const messageSchema = new mongoose.Schema({
+const messageSchema = new mongoose.Schema(
+  {
     senderId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-
     receiverId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-
     text: {
-        type: String
+      type: String,
     },
-
     image: {
-        type: String
+      type: String,
     },
-
-}, 
-    {timestamps: true}
+  },
+  { timestamps: true }
 );
 
 const Message = mongoose.model("Message", messageSchema);
