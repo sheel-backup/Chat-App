2a3f761b7aeb301c2020e099ec264733c7695f5c
Himanshu-Gautam20 <himanshugautam9696@gmail.com>
2025-07-04 14:10:27 +0530

    setting real time conversation with socket.io

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,11 +1,7 @@
-import React from 'react'
-
-const utils = () => {
-  return (
-    <div>
-      utils
-    </div>
-  )
+export function formatMessageTime(date) {
+  return new Date(date).toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
 }
-
-export default utils
