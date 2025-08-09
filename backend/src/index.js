cb7203fc93f621fb760e9ba78a54b9dfe21635be
Himanshu-Gautam20 <himanshugautam9696@gmail.com>
2025-08-09 19:42:46 +0530

    update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,15 +28,8 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// Vercel handles serving the frontend
+// Vercel handles the server lifecycle
+connectDB();
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
-
-server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
-  connectDB();
-});
+export default server;
