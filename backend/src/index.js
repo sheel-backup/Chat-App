cc1cda61fd598b65e3bb4921e0a96fc37124f6d7
sheel-backup <155340880+sheel-backup@users.noreply.github.com>
2025-08-10 19:50:50 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,8 +12,8 @@ import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
+connectDB();
 
-const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json());
@@ -28,7 +28,4 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
-  connectDB();
-});
+export default server;
